d2c92cf1f7098afb7f489c1b676a9e440a1c642b
Jiaolij <jiao.li@paramount.com>
2025-12-03 10:18:19 +0100

    Day 2 - Gift Shop, validate ids

---
+++ Day2/day2.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+// Get input string and transfer to array
+const inputsArr = fs.readFileSync("day2-input.txt", "utf-8")
+                    .split(",")
+                    .filter(Boolean);
+
+const isValidRepeatedID1 = (id) => {
+  // Digits, even length, one group repeated twice
+  return /^(\d+)\1$/.test(id);
+}
+
+const isValidRepeatedID2 = (id) => {
+  // Digits, digit-sequence repeated 2+ times
+  return /^(\d+)\1+$/.test(id);
+}
+
+const finalArr1 = [];
+const finalArr2 = [];
+
+for (const pairs of inputsArr) {
+    const temp = pairs.split("-").filter(Boolean);
+    const start = Number(temp[0]);
+    const end = Number(temp[1]);
+
+    if (temp[0][0] === '0'){
+        return;
+    }
+
+    for (let i = start; i <= end; i++) {
+        if (isValidRepeatedID1(i)) {
+            finalArr1.push(i);
+        }
+
+        if (isValidRepeatedID2(i)) {
+            finalArr2.push(i);
+        }
+    }
+}
+
+const finalSum1 = finalArr1.filter(Boolean).reduce((partialSum, a) => partialSum + a, 0);
+const finalSum2 = finalArr2.filter(Boolean).reduce((partialSum, a) => partialSum + a, 0);
+
+console.log("finalSum - 1:", finalSum1);
+console.log("finalSum - 2:", finalSum2);
